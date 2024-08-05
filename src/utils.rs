b908a38f44bb622149df1de2c003c268a560867b
Bombini <antekbom@gmail.com>
2024-08-05 17:21:15 +0200

    Random Number Utilities

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,15 @@
+use rand::Rng;
+fn random_double() -> f64 {
+    // Returns a random real in [0, 1).
+    let mut rng = rand::thread_rng();
+    rng.gen::<f64>()
+}
+
+fn random_double_range(min: f64, max: f64) -> f64 {
+    // Returns a random real in [min, max).
+    let mut rng = rand::thread_rng();
+    rng.gen_range(min..max)
+}
 fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * std::f64::consts::PI / 180.0
 }
