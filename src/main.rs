b891b16116e881407799d8ee636d690d6bdcee9b
Bombini <antekbom@gmail.com>
2024-08-08 14:56:22 +0200

    Fuzzy metal

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,8 @@ fn main() {
 
     let material_ground = material::Material::Lambertian(Lambertian::new(Vec3::new(0.8, 0.8, 0.0)));
     let material_center = material::Material::Lambertian(Lambertian::new(Vec3::new(0.1, 0.2, 0.5)));
-    let material_mirror = material::Material::Metal(Metal::new(Vec3::new(0.8, 0.8, 0.8)));
-    let material_dark_mirror = material::Material::Metal(Metal::new(Vec3::new(0.8, 0.6, 0.2)));
+    let material_mirror = material::Material::Metal(Metal::new(Vec3::new(0.8, 0.8, 0.8), 0.3));
+    let material_dark_mirror = material::Material::Metal(Metal::new(Vec3::new(0.8, 0.6, 0.2), 1.0));
 
     world.add(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, material_ground)));
     world.add(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.2), 0.5, material_center)));
--- src/material.rs
+++ src/material.rs
@@ -14,7 +14,7 @@ impl Scatterable for Material {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Option<Ray>, Vec3)> {
         match self {
             Material::Lambertian(l) => l.scatter(r_in, rec),
-            Material::Metal(l) => l.scatter(r_in, rec),
+            Material::Metal(m) => m.scatter(r_in, rec),
         }
     }
 }
@@ -44,17 +44,21 @@ impl Scatterable for Lambertian {
 #[derive(Clone)]
 pub struct Metal {
     albedo: Vec3,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Vec3) -> Self {
-        Metal { albedo }
+    pub fn new(albedo: Vec3, fuzz: f64) -> Self {
+        let mut fuzz= fuzz;
+        if !(fuzz < 1.0) { fuzz = 1.0; }
+        Metal { albedo, fuzz }
     }
 }
 
 impl Scatterable for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Option<Ray>, Vec3)> {
-        let reflected = Vec3::reflect(r_in.direction, rec.normal);
+        let mut reflected = Vec3::reflect(r_in.direction, rec.normal);
+        reflected = Vec3::unit_vector(reflected) + (self.fuzz * Vec3::random_unit_vector());
         let scattered = Ray::new(rec.p, reflected);
         let attenuation = self.albedo;
 
